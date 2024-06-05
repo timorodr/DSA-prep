7d7ccb944f7eaaa0a8b4133394dcc1768078c58f
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-06-05 13:14:39 -0500

    squaring and sum of list

---
+++ codewars/Square(n)sum.py
@@ -0,0 +1,33 @@
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 
+# 1
+# 2
+# +
+# 2
+# 2
+# +
+# 2
+# 2
+# =
+# 9
+# 1 
+# 2
+#  +2 
+# 2
+#  +2 
+# 2
+#  =9.
+
+
+numbers = [1,2,2]
+
+def square_sum(numbers):
+    ans = []
+    
+    for n in numbers:
+        square = n * n
+        ans.append(square)
+    return sum(ans)
+
+print(square_sum(numbers))
