a723f30e2def9d4ecf0cc51f120c1d912771d8cb
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-06-07 23:16:30 -0500

    binary search reps

---
+++ neetcode-practice/ReplaceElementswithGreatestElementonRightSide.py
@@ -0,0 +1,50 @@
+# Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+
+# After doing so, return the array.
+
+ 
+
+# Example 1:
+
+# Input: arr = [17,18,5,4,6,1]
+# Output: [18,6,6,6,1,-1]
+# Explanation: 
+# - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+# - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+# - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+# - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+# - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+# - index 5 --> there are no elements to the right of index 5, so we put -1.
+# Example 2:
+
+# Input: arr = [400]
+# Output: [-1]
+# Explanation: There are no elements to the right of index 0.
+ 
+
+# Constraints:
+
+# 1 <= arr.length <= 104
+# 1 <= arr[i] <= 105
+
+from typing import List
+
+nums = list(range(0, 1001))
+target = 325
+
+class Solution:
+    def find_target(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+    
+solution = Solution()
+print(solution.find_target(nums, target))
