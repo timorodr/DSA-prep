b0887ad25c455d4d0049e4229f387b22afd058b8
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-07-05 23:23:49 -0500

    group anagram cont

--- neetcode-practice/GroupAnagrams.py
+++ neetcode-practice/GroupAnagrams.py
@@ -28,10 +28,16 @@
 from typing import List
 from collections import defaultdict
 
+strs = ["eat","tea","tan","ate","nat","bat"]
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
-
+        print(res)
         for s in strs:
-            res["".join(sorted(s))].append(s)
+            print(res["".join(sorted(s))].append(s)) #sort the key and append s if key sorted is same as key
+            print(res)
         return res.values()
+    
+solution = Solution()
+print(solution.groupAnagrams(strs))
