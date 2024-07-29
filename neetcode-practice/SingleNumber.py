c23a20cb785570d42218c6e1a545ba321022e479
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-07-28 22:55:24 -0500

    single number

---
+++ neetcode-practice/SingleNumber.py
@@ -0,0 +1,51 @@
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,2,1]
+# Output: 1
+# Example 2:
+
+# Input: nums = [4,1,2,1,2]
+# Output: 4
+# Example 3:
+
+# Input: nums = [1]
+# Output: 1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -3 * 104 <= nums[i] <= 3 * 104
+# Each element in the array appears twice except for one element which appears only once.
+
+
+
+
+from typing import List
+
+nums = [4,1,2,1,2]
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashmap = {}
+
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            elif num not in hashmap:
+                hashmap[num] = 1
+            else:
+                return
+
+        for key, value in hashmap.items():
+            if value == 1:
+                return key
+            
+solution = Solution()
+print(solution.singleNumber(nums))
