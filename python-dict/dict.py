043a1b2be2facf6a1385131ea01ee9e547fb84d2
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-08-04 23:56:48 -0500

    dict methods

--- python-dict/dict.py
+++ python-dict/dict.py
@@ -37,3 +37,16 @@ res_string["hello"] = "mars"
 print(res_list, "LIST")
 print(res_int, "INT")
 print(res_string, "STRING")
+
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+res = defaultdict(list) # dict of lists for values
+
+
+for words in strs: # loop through array 
+    res["".join(sorted(words))].append(words) #create key of each sorted word and append word.
+    #if key already exists the sorted word will append to corresponding key
+    # if key does not exist, the key will be created and append the word to its sorted version
+    # return the values (LISTS) of all the keys
+    print(res)
+print(res.values())
