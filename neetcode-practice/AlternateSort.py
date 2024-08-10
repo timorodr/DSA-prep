068a681b0f363debedfdaa357f033926c43f0e8a
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-08-09 23:43:38 -0500

    alt sort

---
+++ neetcode-practice/AlternateSort.py
@@ -0,0 +1,17 @@
+
+from typing import List
+
+nums = [1,2,3,4,5,6,7,8]
+
+class Solution:
+    def altSort(self, nums: List[str]) -> int:
+
+        res = []
+
+        for i in range(0, len(nums), 2):
+            res.append(nums[i])
+        
+        return res
+
+solution = Solution()
+print(solution.altSort(nums))
