b2b10f831402a948b6cf83ee3d003d7a793e1579
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-08-14 22:57:56 -0500

    3sum

---
+++ neetcode-practice/3Sum.py
@@ -0,0 +1,26 @@
+from typing import List
+
+nums = [-4, -2, -1, -1, 0, 1, 2]
+target = 0
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+        return res
