7e64f513d943f5f317f20b80a327807091852cf4
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-08-26 22:24:24 -0500

    sub_len str occurences

--- neetcode-practice/OccurencesOfStr2inStr1.py
+++ neetcode-practice/OccurencesOfStr2inStr1.py
@@ -9,11 +9,12 @@ class Solution:
         count = 0
 
         for i in range(len(string1)):
-            if string1[i: i + sub_len] == string2:
-                count+= 1
+            if string1[i:i + sub_len] == string2:
+                count += 1
         return count
-    
 
 solution = Solution()
 print(solution.stringInString(string1, string2))
+
+## test cases may need .lower()
 
