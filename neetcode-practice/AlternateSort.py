2b6963b97023b6d5bad5f4aa5a1d652cdb7e7b11
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-08-27 22:57:49 -0500

    alt sort slice

--- neetcode-practice/AlternateSort.py
+++ neetcode-practice/AlternateSort.py
@@ -6,12 +6,12 @@ nums = [1,2,3,4,5,6,7,8]
 class Solution:
     def altSort(self, nums: List[str]) -> int:
 
-        res = []
+        # res = []
 
-        for i in range(0, len(nums), 2):
-            res.append(nums[i])
+        # for i in range(0, len(nums), 2):
+        #     res.append(nums[i])
         
-        return res
+        return nums[::2]
 
 solution = Solution()
 print(solution.altSort(nums))
