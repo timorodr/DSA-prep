1b38936971723b5d56162865ae84be8d9a39b144
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-09-03 22:46:36 -0500

    divisible by 60 num pairs

---
+++ neetcode-practice/numPairsDivisibleBy60.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+
+
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        res = 0
+        timeMap = {}
+
+        for t in time:
+            rem = t % 60 
+
+            if rem == 0:
+                res += timeMap.get(rem, 0)
+            else:
+                res += timeMap.get(60 - rem, 0)
+                
+
+            timeMap[rem] = timeMap.get(rem, 0) + 1
+        return res
