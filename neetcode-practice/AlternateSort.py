d71c2c38f3972de72513a9415978cdf4b191e78c
timorodr <tim.rdrgz2017@live.com>
2024-10-06 23:13:19 -0500

    container most water

--- neetcode-practice/AlternateSort.py
+++ neetcode-practice/AlternateSort.py
@@ -5,13 +5,9 @@ nums = [1,2,3,4,5,6,7,8]
 
 class Solution:
     def altSort(self, nums: List[str]) -> int:
-
-        # res = []
-
-        # for i in range(0, len(nums), 2):
-        #     res.append(nums[i])
         
-        return nums[::2]
+        for i in range(0, len(nums), 2):
+            print(nums[i])
 
 solution = Solution()
 print(solution.altSort(nums))
